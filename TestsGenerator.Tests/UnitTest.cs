4f246f90232e5691a9ccc62322b1aa3a123052d8
evensky1 <fromthedeepsize@gmail.com>
2022-11-02 14:49:09 +0300

    Generator logic was upgraded

--- TestsGenerator.Tests/UnitTest.cs
+++ TestsGenerator.Tests/UnitTest.cs
@@ -23,13 +23,75 @@ public class Tests
                          class Program
                          {
                              static void Main(string[] args)
-                              {
+                             {
                                  Console.WriteLine(""Hello, World!"");
                              }
+
+                             public void Method1() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method2() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method3() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+
+                             public void Method4() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }            
+                         }
+                     }
+                     namespace MyHelloWorld.AnotherOne
+                     {
+                         class MyAnotherProgram
+                         {
+                             public void Method5() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method6() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method7() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+
+                             public void Method8() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }            
+                         }
+                         class Something
+                         {
+                             public void Method9() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method10() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+                             public void Method11() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }     
+
+                             public void Method12() 
+                             {
+                                Console.WriteLine(""Hello, World!"");
+                             }            
                          }
                      }";
         
-        gen.GenerateAsync(code);
+        var str = gen.GenerateAsync(code).Result;
+        Console.WriteLine(str);
         
         Assert.That(true);
     }
